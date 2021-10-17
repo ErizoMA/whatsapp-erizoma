0fd014d9273c39579590bdf329c8a042f28ac2c9
Erick <marcerican@gmail.com>
2021-10-16 21:39:46 -0500

    Add a random avatar to every Chat

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,23 +1,30 @@
 import { Avatar } from "@mui/material";
-import React, { useEffect, useState } from "react"
-import "../styles/SidebarChat.css"
+import React, { useEffect, useState } from "react";
+import "../styles/SidebarChat.css";
 
-function SidebarChat({addNewChat}) {
+function SidebarChat({ addNewChat }) {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
 
-  const [seed,setSeed] = useState("");
-
-  useEffect(()=>{
-    setSeed(Math.floor(Math.random()*5000))
-  },[])
-  return ( 
-  <div className="sidebarChat">
-    <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-    <div className="sidebarChat__info">
-      <h2>Room name</h2>
-      <p>Last message ...</p>
+  const createChat = () => {
+    const roomName = prompt("Please enter name for Chat");
+    if (roomName){}
+  };
+  return !addNewChat ? (
+    <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last message ...</p>
+      </div>
     </div>
-  </div>
-   );
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new Chat</h2>
+    </div>
+  );
 }
 
 export default SidebarChat;
